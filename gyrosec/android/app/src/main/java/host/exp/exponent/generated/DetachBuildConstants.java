a4c2b51c88eeb5f738d3030ab6d6d17cbaaf493b
Jason Seibel <jasonLseibel@gmail.com>
2019-05-08 20:14:17 -0400

    app works now in ejected form

---
+++ gyrosec/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp30f58b10a9494101be9b9c6fd2d5f4f6://18.21.130.140:19000";
+
+}
